9d21d373aaa5cbe305d6e5b6ba9b57750e9ca4b1
Schouwey <zaid.schouwey@gmail.com>
2025-01-19 20:22:40 +0100

    all in one

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3333',
   }));
-app.use(express.json());
+
+// serve static assets normally
+app.use(express.static('www'))
+
 app.use('/items', itemRoutes);
 app.use('/categories', categoryRoutes);
 app.use('/rootcategories', rootCategoryRoutes);
@@ -21,5 +24,8 @@ app.use('/recipes', recipeRoutes);
 app.use('/itemrecipes', itemRecipeRoutes);
 app.use('/itemsaverageprice', averageRoutes);
 app.use('/exactprices', exactPriceRoutes);
+
+
+
 const PORT = 2989;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ www/build/index.esm.js
@@ -0,0 +1,2 @@
+import{c as o}from"./p-2f3d151d.js";import"./p-59902f89.js";const p=o();export{p as Router};
+//# sourceMappingURL=index.esm.js.map
